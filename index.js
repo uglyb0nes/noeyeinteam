ab4e5552acf3f4a110f1b177d62e78f56932354d
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-08-06 20:40:54 -0700

    members html

--- index.js
+++ index.js
@@ -78,3 +78,26 @@ function member() {
         })
     });
 };
+
+function html() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <title>Team Member Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Member Profile</span>
+        </nav>
+        <div class="card-group">`;
+    fs.writeFile("./dist/members.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("Initiate");
+}
